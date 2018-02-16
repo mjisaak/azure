0b311510136c649883efb2df9dc55b827722fcd8
Martin Brandl <mjisaak@users.noreply.github.com>
2018-02-16 06:08:22 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,7 @@
+
+$profilePath = 'https://raw.githubusercontent.com/mjisaak/azure/master/profile.ps1'
+((New-Object System.Net.WebClient).DownloadString($profilePath)) | Set-Content $profile -Force
+
 function Say-Hello 
 {
   Write-Host "This works."
