49f02a7ffc0ccc6f6767888ecaad32f79a9d884f
Martin Brandl <mjisaak@users.noreply.github.com>
2018-02-16 20:44:30 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,3 +2,24 @@
  {
      Write-Host "hello, world!"
  }
+
+function Remove-EmptyAzureResourceGroups
+{
+    [CmdletBinding(SupportsShouldProcess=$true)]
+    Param ()
+        
+    $resourceGroups = Get-AzureRmResourceGroup -WarningAction SilentlyContinue
+    $resources = Get-AzureRmResource
+ 
+    $nonEmptyResourceGroups = $resourceGroups | foreach {
+        $resources | ? ResourceGroupName -eq $_.ResourceGroupName | select -expand ResourceGroupName -Unique 
+    }        
+    $ResourceGroupsToDelete = $resourceGroups | where { $_.ResourceGroupName -notin $nonEmptyResourceGroups }
+ 
+    $ResourceGroupsToDelete | foreach {
+        if ($pscmdlet.ShouldProcess($_.ResourceGroupName, 'Remove-AzureRmResourceGroup'))
+        {
+            $_ | Remove-AzureRmResourceGroup -Confirm:$false
+        }
+    }   
+}
