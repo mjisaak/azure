2820292b08c31de66a12bc6509f35a021ea317c9
Martin Brandl <mjisaak@users.noreply.github.com>
2018-02-16 07:16:07 +0100

    Update Set-Profile.ps1

--- Set-Profile.ps1
+++ Set-Profile.ps1
@@ -1,6 +1,7 @@
 <# 
 # Execute the following line to persist the file in your profile:
-(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mjisaak/azure/master/Set-Profile.ps1') | Set-Content $profile -Force
+(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mjisaak/azure/master/Set-Profile.ps1') | 
+  Set-Content $profile -Force
 #>
 
 $profilePath = 'https://raw.githubusercontent.com/mjisaak/azure/master/profile.ps1'
