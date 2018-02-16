d641130ed7c3d37fc5379d7d198c6be0824936ce
Martin Brandl <mjisaak@users.noreply.github.com>
2018-02-16 06:15:09 +0100

    Update Set-Profile.ps1

--- Set-Profile.ps1
+++ Set-Profile.ps1
@@ -1,5 +1,4 @@
 $profilePath = 'https://raw.githubusercontent.com/mjisaak/azure/master/profile.ps1'
 
 $downloadString = '{0}?{1}' -f $profilePath, (New-Guid)
-((New-Object System.Net.WebClient).DownloadString($profilePath)) | 
-    Set-Content $profile -Force
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($profilePath))
