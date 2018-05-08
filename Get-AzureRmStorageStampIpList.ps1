5cdb92e26f90972728bdcff2ba7304e26217abda
Martin Brandl <mjibrandl@gmail.com>
2018-05-08 14:37:06 +0200

    Added script to retrieve all storage stamp ips

---
+++ Get-AzureRmStorageStampIpList.ps1
@@ -0,0 +1,11 @@
+﻿Param
+(
+    [Parameter(Mandatory=$true, Position=0)]
+    $ResourceGroupName
+)
+Get-AzureRmStorageAccount -ResourceGroupName $resourceGroupName | ForEach-Object {
+    [PSCustomObject]@{
+        StorageAccount = $_.StorageAccountName
+        IPAddress = [System.Net.Dns]::GetHostAddresses((([Uri]$_.PrimaryEndpoints.Blob).Host)).IPAddressToString
+    }
+}
