d3a1cd2717b86d04d0d9b295e632f804ad5c5c5c
Martin Brandl <mjibrandl@gmail.com>
2018-06-12 15:15:04 +0200

    Added additional parameter set to Get-AzureRmResourceProviderLatestApiVersion

--- Get-AzureRmResourceProviderLatestApiVersion.ps1
+++ Get-AzureRmResourceProviderLatestApiVersion.ps1
@@ -5,45 +5,56 @@
    The following cmdlet returns the latest API version for the specified resource provider. 
    You can also include pre-release (preview) versions using the -IncludePreview switch
 .EXAMPLE
+   Using the Full Parameter Set:
    Get-AzureRmResourceProviderLatestApiVersion -Type Microsoft.Storage/storageAccounts
 .EXAMPLE
+   Using the Full Parameter Set with the -IncludePreview switch:
    Get-AzureRmResourceProviderLatestApiVersion -Type Microsoft.Storage/storageAccounts -IncludePreview
+.EXAMPLE
+   Using the ProviderAndType Parameter Set:
+   Get-AzureRmResourceProviderLatestApiVersion -ResourceProvider Microsoft.Storage -ResourceType storageAccounts
 #>
-function Get-AzureRmResourceProviderLatestApiVersion
-{
+function Get-AzureRmResourceProviderLatestApiVersion {
     [CmdletBinding()]
     [Alias()]
     [OutputType([string])]
     Param
     (
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(ParameterSetName = 'Full', Mandatory = $true, Position = 0)]
         [string]$Type,
+
+        [Parameter(ParameterSetName = 'ProviderAndType', Mandatory = $true, Position = 0)]
+        [string]$ResourceProvider,
+
+        [Parameter(ParameterSetName = 'ProviderAndType', Mandatory = $true, Position = 1)]
+        [string]$ResourceType,
 
         [switch]$IncludePreview
     )
 
     # retrieving the resource providers is time consuming therefore we store
     # them in a script variable to accelerate subsequent requests.
-    if (-not $script:resourceProvider) 
-    {
-         $script:resourceProvider = Get-AzureRmResourceProvider   
+    if (-not $script:resourceProvider) {
+        $script:resourceProvider = Get-AzureRmResourceProvider   
     }
 
-    $provider = ($Type -replace "\/.*")
-    $typeName = ($Type -replace ".*?\/(.+)", '$1')
-
-      $provider = $script:resourceProvider | 
+    if ($PSCmdlet.ParameterSetName -eq 'Full') {
+        $ResourceProvider = ($Type -replace "\/.*")
+        $ResourceType = ($Type -replace ".*?\/(.+)", '$1')
+    }
+    
+    $provider = $script:resourceProvider | 
         Where-Object {
-            $_.ProviderNamespace -eq $provider -and 
-            $_.ResourceTypes.ResourceTypeName -eq $typeName
-        }
+        $_.ProviderNamespace -eq $ResourceProvider -and 
+        $_.ResourceTypes.ResourceTypeName -eq $ResourceType
+    }
 
     if ($IncludePreview) {
         $provider.ResourceTypes.ApiVersions[0]
     }
     else {
         $provider.ResourceTypes.ApiVersions | Where-Object {
-                $_ -notmatch '-preview'
-              } | Select-Object -First 1
+            $_ -notmatch '-preview'
+        } | Select-Object -First 1
     }
 }
