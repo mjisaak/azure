5950dc6237838de8b4c4b2e0d13bc3f58b52bb7c
Martin Brandl <martin.brandl@whiteduck.de>
2018-11-02 10:11:06 +0100

    Initial AzureStorageExample commit

---
+++ Storage/AzureStorageExamples/AzureStorageExamples/Options/StorageAccount.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureStorageExamples.Options
+{
+    public class StorageAccount
+    {
+       public string ConnectionString { get; set; }
+       public string ContainerName { get; set; }
+    }
+}
---
+++ Storage/AzureStorageExamples/AzureStorageExamples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureStorageExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            Console.WriteLine(configuration.GetConnectionString("Storage"));
+        }
+    }
+}
